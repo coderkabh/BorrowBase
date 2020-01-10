8103ba2b0a46d1405b8a1d68a7c4727c82a459be
kanishk <kanishkbharadwaj333@gmail.com>
2020-01-11 01:04:30 +0530

    added createNewEntryInCustomer() method and isNewEnterInCustomerCreated() method

--- src/DatabaseMethods/CustomerDataMethods.java
+++ src/DatabaseMethods/CustomerDataMethods.java
@@ -1,5 +1,33 @@
 package DatabaseMethods;
 
-public class CustomerDataMethods {
+import data.Customer;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class CustomerDataMethods {
+    Scanner scanner = new Scanner(System.in);
+    private boolean isQueryPerformed = false;
+
+    public void createNewEntryInCustomer(Connection connection, Customer customer) throws SQLException {
+        String queryStatement = "INSERT INTO CUSTOMER (c_id,c_name,c_add,c_phno) VALUES (?,?,?,?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(queryStatement);
+        preparedStatement.setString(1, customer.getC_id());
+        preparedStatement.setString(2, customer.getC_name());
+        preparedStatement.setString(3, customer.getC_add());
+        preparedStatement.setString(4, customer.getC_phno());
+
+        isQueryPerformed = preparedStatement.executeUpdate() >= 1;
+
+    }
+
+    public boolean isNewEnterInCustomerCreated() {
+        if (isQueryPerformed) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
