2c772bb4b0fd8a9a2ae892d03dc43180f716b943
kanishk <kanishkbharadwaj333@gmail.com>
2020-01-11 00:31:56 +0530

    added Constructor

--- src/data/Transaction.java
+++ src/data/Transaction.java
@@ -1,4 +1,13 @@
 package data;
 
 public class Transaction {
+    private String c_id;
+    private int deposited;
+    private int rem_amt;
+
+    public Transaction(String c_id, int deposited, int rem_amt) {
+        this.c_id = c_id;
+        this.deposited = deposited;
+        this.rem_amt = rem_amt;
+    }
 }
