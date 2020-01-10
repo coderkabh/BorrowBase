539e5cd536b031506464adfbc1cbe0994ccf9462
kanishk <kanishkbharadwaj333@gmail.com>
2020-01-11 00:20:11 +0530

    completed class DatabaseConnection.java and fixed a bug of URL String

--- src/connection/DatabaseConnection.java
+++ src/connection/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/BorrowBaseDB";
     private static final String USERNAME = "";
     private static final String PASSWORD = "";
     private static Connection connection = null;
