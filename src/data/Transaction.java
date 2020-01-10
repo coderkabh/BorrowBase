c71ed9907b3b08752ce6eac8a7dc073652ed3444
kanishk <kanishkbharadwaj333@gmail.com>
2020-01-11 00:32:32 +0530

    added getters and setters

--- src/data/Transaction.java
+++ src/data/Transaction.java
@@ -10,4 +10,28 @@ public class Transaction {
         this.deposited = deposited;
         this.rem_amt = rem_amt;
     }
+
+    public String getC_id() {
+        return c_id;
+    }
+
+    public void setC_id(String c_id) {
+        this.c_id = c_id;
+    }
+
+    public int getDeposited() {
+        return deposited;
+    }
+
+    public void setDeposited(int deposited) {
+        this.deposited = deposited;
+    }
+
+    public int getRem_amt() {
+        return rem_amt;
+    }
+
+    public void setRem_amt(int rem_amt) {
+        this.rem_amt = rem_amt;
+    }
 }
